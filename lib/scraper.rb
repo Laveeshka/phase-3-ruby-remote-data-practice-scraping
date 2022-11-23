8ba56d30d0caa0a4288ebd65b6617846e8aba43d
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-11-24 00:26:13 +1100

    learn how to use Nokogiri gem to scrape data from HTML documents

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,3 +2,13 @@ require 'nokogiri'
 require 'open-uri'
 
 html = open("https://flatironschool.com/")
+
+doc = Nokogiri::HTML(html)
+doc.css(".heading-financier").text.strip
+
+courses = doc.css(".heading-35-semibold")
+courses.each do |course|
+    puts course.text.strip
+end
+
+p doc.css(".heading-35-semibold")[0]
